25e768196d13e738f4508ec08cdb989a39c079a3
supersliser <43965667+supersliser@users.noreply.github.com>
2024-02-18 19:25:18 +0000

    Revert "Vercel SSR installed"
    This reverts commit c7140925d3f089cedcbfe1b47e774a9a0f3417b8.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: "server",
-  adapter: vercel()
+  integrations: [react()]
 });
