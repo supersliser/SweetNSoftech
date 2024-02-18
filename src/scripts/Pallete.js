26bbdb5252489b04f9de7b98f7b9d200e6a54a22
supersliser <43965667+supersliser@users.noreply.github.com>
2024-02-18 15:05:35 +0000

    Background Animation Attempt 1

---
+++ src/scripts/Pallete.js
@@ -0,0 +1,8 @@
+let colourScheme = [""];
+colourScheme[0] = ["rgb(176, 224, 230)", "powderBlue"];
+colourScheme[1] = ["rgb(204, 204, 255)", "periwinkle"];
+colourScheme[2] = ["rgb(43, 69, 147)", "marianBlue"];
+colourScheme[3] = ["rgb(173, 216, 230)", "lightBlue"];
+colourScheme[4] = ["rgb(0,123,167)", "cerulean"];
+colourScheme[5] = ["rgb(102,148,203)", "iceberg"];
+export default colourScheme;
